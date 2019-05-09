98053c229da32701d21472214a3d107c7186979c
vybhavb <vybhavbugs+github@gmail.com>
2019-05-09 12:15:33 -0700

    Fixed template

---
+++ firebase.js
@@ -0,0 +1,7 @@
+//Paste the details given to you by firebase under this comment
+//eg
+// var firebaseConfig = {
+// ...config
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-//Paste the details given to you by firebase under this comment
-//eg
-// var firebaseConfig = {
-// ...config
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+function handleAuthChanges() {
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      window.location = "index.html";
+    } else {
+      //TODO sign out
+    }
+  });
+}
+
 
 function handleSignIn() {
   const email= document.getElementById("email").value
